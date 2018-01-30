2ed2441bf608b252a23cdc88299e6fef154075a1
Your Name <you@example.com>
2018-01-30 18:24:32 +0100

    Git init

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :zamowienias
   resources :produkties
   resources :produkty_na_zamowienius
+  root to: 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
